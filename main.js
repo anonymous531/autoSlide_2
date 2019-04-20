ae8c2ee9e732de953b62eab8372f9f6cec7b8d3a
tjr <2871779035@qq.com>
2019-04-20 21:46:01 +0800

    third-commit

--- main.js
+++ main.js
@@ -1,17 +1,48 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-let n=1
-setInterval(function(){
-    $(`.images > img:nth-child(${justifyN(n)})`).removeClass('current').addClass('leave')
-    .one('transitionend',function(e){
-        $(e.currentTarget).removeClass('leave').addClass('enter')
+initial()
+var allButtons = $('.buttons > button')
+var n=1
+var timer = setInterval(function(){
+    makeLeave(getImage(n)).one('transitionend',function(e){
+        makeEnter($(e.currentTarget))
     })
-    $(`.images > img:nth-child(${justifyN(n+1)})`).removeClass('enter').addClass('current')
+    $(`.buttons > button:nth-child(${justifyN(n+1)})`).addClass('pink').siblings().removeClass('pink')
+    makeCurrent(getImage(n+1))
     n++
-},3000)
+},2000)
 
 
+
+$('#window').on('mouseenter',function(e){
+    window.clearInterval(timer)
+})
+$('#window').on('mouseleave',function(e){
+    setInterval(function(){
+        makeLeave(getImage(n)).one('transitionend',function(e){
+            makeEnter($(e.currentTarget))
+        })
+        $(`.buttons > button:nth-child(${justifyN(n+1)})`).addClass('pink').siblings().removeClass('pink')
+        makeCurrent(getImage(n+1))
+        n++
+    },2000)
+})
+
+
+function initial(){
+    $('.images > img:nth-child(1)').addClass('current').siblings().addClass('enter')
+    $('.buttons > button:nth-child(1)').addClass('pink')
+}
+function makeLeave(node){
+    return node.removeClass('current').addClass('leave')
+}
+function makeEnter(node){
+    return node.removeClass('leave').addClass('enter')
+}
+function makeCurrent(node){
+    return node.removeClass('enter').addClass('current')
+}
+function getImage(n){
+    return $(`.images > img:nth-child(${justifyN(n)})`)
+}
 function justifyN(n){
     if(n>3){
         n=n%3
