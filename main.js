73e93158a9d775a25b126fe2c80460accb591458
tjr <2871779035@qq.com>
2019-04-20 15:43:51 +0800

    第一个版本

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ setInterval(function(){
     $(`.images > img:nth-child(${justifyN(n+1)})`).removeClass('enter').addClass('current')
     n++
 },3000)
+
+
 function justifyN(n){
     if(n>3){
         n=n%3
